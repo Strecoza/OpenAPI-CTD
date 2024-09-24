f295f5b5bb95f2973a122faf0922dd188b318de3
TatianaPri <87344989+Strecoza@users.noreply.github.com>
2024-09-24 16:26:04 -0700

    solved problem with btn, css left

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,10 @@
-//fetch 
+
 
 const artworksContainer = document.getElementById("artworks_container");
 const oneArtworkContainer = document.getElementById("one_artwork_container");
 const artworkInfo = document.getElementById("artwork_info");
 
-
+//Fetch request 
 const apiUrl = 'https://api.artic.edu/api/v1/artworks?page=2&limit=20';
 fetch (apiUrl)
 .then((response) => {
@@ -14,47 +14,50 @@ fetch (apiUrl)
         throw new Error ('failed');
     }
 })
-.then((data) =>{
+.then((data) => {
     const artworks = data.data;
-    console.log(artworks)
 
-artworks.forEach(artwork => {
-    let art = document.createElement("li");
-    art.innerText = artwork.title;
-    art.style.cursor = 'pointer';    artworkInfo.appendChild(art);
+    artworks.forEach(artwork => {
+        let art = document.createElement("li");
+        art.innerText = artwork.title;
+        art.style.cursor = 'pointer';    
+        artworkInfo.appendChild(art);
 
-    art.addEventListener('click', () =>{
-        displayArtworkData(artwork, 'one_artwork_container') 
+        art.addEventListener('click', () => {
+            displayArtworkData(artwork, 'one_artwork_container') 
+        })
     })
 })
-})
-.catch((error) =>{
+.catch((error) => {
     console.error('error', error);
 });
 //Display data
 
 function displayArtworkData(artworkData, elementId){
     const artworkElement = document.getElementById(elementId);
+    
     if(artworkData){
         artworkElement.innerHTML = `<img src = "https://www.artic.edu/iiif/2/${artworkData.image_id}/full/200,/0/default.jpg" alt = "${artworkData.title}"/>
         <p><strong>Title:</strong>${artworkData.title}</p>
         <p><strong>Artist:</strong>${artworkData.artist_title}</p>
         <p><strong>Description:</strong>${artworkData.description||'No description to show'}</p>
         <p><strong>Place of Origin:</strong>${artworkData.place_of_origin||'Unknown'}</p>
+        <br>
+        <button id = "back_button">Go back</button>
         `;
+
+//Button
+        document.getElementById('back_button').onclick = () => {
+            artworksContainer.style.display = 'block';
+            oneArtworkContainer.style.display = 'none';
+        }
+
     }else{
         artworkElement.innerHTML = '<p>Sorry, nothing to show you</p>'
     }
     artworksContainer.style.display = 'none';
     oneArtworkContainer.style.display = 'block';
     }
-
-    //Button
-    document.querySelector('.back_button').onClick = () => {
-        artworksContainer.style.display = 'block';
-        oneArtworkContainer.style.display = 'none';
-    }
-
 
 //----FOOTER----//
 const footer = document.querySelector('footer');
